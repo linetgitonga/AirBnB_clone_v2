d02e7a8e9cdb59a8f8247d9083ad13e3b7fea18f
linetgitonga <linetgitonga55@gmail.com>
2024-04-04 20:04:27 -0700

    changes

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
+# Fabfile to delet thee out-of-date archives.
 import os
 from fabric.api import *
 
 env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
+
 def do_clean(number=0):
     """Delete out-of-date archives.
 
-    Args:
-        number (int): The number of archives to keep.
+    Args:        number (int): The number of archives to keep.
 
     If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
+    number is 2, keeps the most and second-most recent archives,.
+    Any mosst recent becomes the active one
     """
     number = 1 if int(number) == 0 else int(number)
 
--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,7 @@
 # Configures a web server for deployment of web_static.
 
 # Nginx configuration file
+
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
